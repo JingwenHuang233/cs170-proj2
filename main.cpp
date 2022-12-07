fe55f99e4d21709fa073b9ea48fa44a4a0f5ce75
JingwenHuang233 <jingwenhuang233@gmail.com>
2022-12-07 01:19:33 +0000

    finish forward selection

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
-
+double leave_one_out_cross_validation(vector<vector<double>> data, vector<int> current_set, int feature_to_add);
+void forward(vector<vector<double>> data);
+void backward(vector<vector<double>> data);
 int main(int argc, char** argv){
     ifstream file;  //https://linuxhint.com/read-text-file-2d-array-cpp/
     file.open(argv[1]);
@@ -22,6 +25,60 @@ int main(int argc, char** argv){
 
     }
     file.close();
+    int choice;
+    cout<<"Choose search algorithm:\n1-Forward Selection\n2-Backward Elimination\n";
+    cin>>choice;
+    switch(choice){
+        case 1: 
+            forward(data);
+            break;
+        case 2:
+            backward(data);
+            break;
+    }
     
     return 0;
+}
+
+double leave_one_out_cross_validation(vector<vector<double>> data, vector<int> current_set, int feature_to_add){
+    double accuracy = rand()/RAND_MAX;
+    return accuracy;
+}
+
+void forward(vector<vector<double>> data){
+    int numFeatures = data[0].size();
+    vector<int> current_set_of_features;
+    double best_accuracy = 0;
+    vector<int> best_features_set;
+    for (int i = 1; i< numFeatures; ++i){
+        cout<< "On the "<<i<<" th level of of the search tree"<<endl;
+        int feature_to_add_at_this_level;
+        double best_so_far_accuracy = 0;
+        for (int j = 1; j< numFeatures; ++j){
+            if (find(current_set_of_features.begin(), current_set_of_features.end(), j) == current_set_of_features.end()){
+                cout<< "--Considering adding feature "<<j<<endl;
+                double accuracy = leave_one_out_cross_validation(data,current_set_of_features,j);
+                if (accuracy > best_so_far_accuracy){
+                    best_so_far_accuracy = accuracy;
+                    feature_to_add_at_this_level = j;
+                }
+            }
+        }
+        current_set_of_features.push_back(feature_to_add_at_this_level);
+        cout<< "On level "<<i<<", I added feature "<< feature_to_add_at_this_level<<endl;
+        if(best_so_far_accuracy>best_accuracy){
+            best_accuracy = best_so_far_accuracy;
+            best_features_set = current_set_of_features;
+        }
+    }
+    cout<<"The best feature set is {"<<best_features_set[0];
+    for(int i = 1; i<best_features_set.size(); i++){
+        cout<<","<<best_features_set[i];
+    }
+    cout<<"}, which has an accuracy of "<<best_accuracy*100<<"%\n";
+}
+
+void backward(vector<vector<double>> data){
+    
+
 }
